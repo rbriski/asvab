75603637afc610ab0201f88ff5a47ff1a162466d
Bob Briski <bbriski@wpcn10.suw.corp.google.com>
2007-11-29 17:10:38 -0800

    Filled out workflow
    * Added docs
    * Workflow runs all jobs correctly
    * Added latest symlink
    * Started adding pid for currently running flow

--- job.py
+++ job.py
@@ -14,23 +14,67 @@ class Job(object):
     values to a file.  Also keeps stats of the job.
 
     Stats include:
-      script location
-      start time
-      end time
-      return value
-      whether the job is running
-      if the job succeeded
-      if the job is done
-    """
-    root = os.getcwd()
-    statKeys = ['jobId', 'startTime', 'endTime', 'retCode']
+      - script location
+      - start time
+      - end time
+      - return value
+      - whether the job is running
+      - if the job succeeded
+      - if the job is done
 
-    def __init__(self, jobId, script, logDir=None):
+    @cvar statKeys: variables that will have their values dumped into
+    a stats table upon completion of the job
+    @type statKeys: C{list}
+    @ivar jobId: the id of the current job (not a process id)
+    @type jobId: C{string}
+    @ivar logDir: the directory where the log files from this job
+    are dumped
+    @type logDir: C{string}
+    @ivar homeDir: the home directory of this job, used as CWD in the environment
+    @type homeDir: C{string}
+    @ivar script: Actual command to run the job
+    @type script: C{string} or C{list}
+    @ivar needsSuccess: Whether this job needs its parent to successfully execute to run
+    @type needsSuccess: C{boolean}
+    @ivar env: Environment of the job
+    @type env: C{dict}
+    @ivar done: If the job is done
+    @type done: C{boolean}
+    @ivar running: If the job is running
+    @type running: C{boolean}
+    @ivar succeeded: If the job succeeded
+    @type succeeded: C{boolean}
+    @ivar starved: If the jobs parents failed
+    @type starved: C{boolean}
+    
+    """
+    statKeys = ['jobId', 'startTime', 'endTime', 'retCode', 'starved', 'succeeded']
+
+    def __init__(self, jobId, script, logDir=None, homeDir=None, env=None):
+        """
+        Initializes but DOES NOT run the job
+
+        @param jobId: The id or name of the job
+        @type jobId: C{string}
+        @param script: The actual command to run
+        @type script: C{string} or C{list}
+        @keyword logDir: The directory where log files will be created
+        @type logDir; C{string}
+        @keyword homeDir: The CWD of this job
+        @type homeDir: C{string}
+        @keyword env: The environment of this job
+        @type env: C{dict}
+        """
         self.jobId = jobId
+        
+        if logDir is None:
+            logDir=os.getcwd()
         self.logDir = logDir
 
+        self.homeDir = homeDir
+
         #Script file location
-        self.script = os.path.join(self.root, script)
+        self.script = script.split()
 
         #If this script needs its parent to succeed
         #TODO: Move this out into the workflow manager
@@ -39,15 +83,17 @@ class Job(object):
         self.needsSuccess = True
 
         #Out, error, and stat files
-        self.out = None
-        self.err = None
-        self.stat = None
+        self._out = self._getFile('out')
+        self._err = self._getFile('err')
+        self._stat = self._getFile('stat')
 
         #Stats
         self.startTime = None
         self.pid = None
         self.endTime = None
         self.retCode = None
+
+        self.env = env
 
         #The actual running process
         self.job = None
@@ -58,18 +104,26 @@ class Job(object):
         self.succeeded = False
         self.starved = False
 
-    def getFile(self, ts, extension):
+    def __del__(self):
+        """ Deconstructor
+        """
+        #Close the filehandles
+        self._out.close()
+        self._err.close()
+        self._stat.close()
+
+
+
+    def _getFile(self, extension):
         """ Opens a file based on the timestamp and extension
         """
-        fileLoc = self.getFileName(ts, extension)
+        fileLoc = self._getFileName(extension)
         return open(fileLoc, 'w')
 
-    def getFileName(self, ts, extension):
+    def _getFileName(self, extension):
         """ Gets a filename based on a timestamp and extension
         """
-        fileName = '.'.join([str(self.jobId), str(ts), extension])
-        if self.logDir is None:
-            self.logDir = self.root
+        fileName = '.'.join([str(self.jobId), extension])
         fileLoc = os.path.join(self.logDir, fileName)
         return fileLoc
     
@@ -81,57 +135,62 @@ class Job(object):
         Runs the script
         """
         startTS = int(time.time())
-        self.out = self.getFile(startTS, 'out')
-        self.err = self.getFile(startTS, 'err')
-        self.stat = self.getFile(startTS, 'stat')
-
         self.startTime = datetime.fromtimestamp(startTS)
 
-        print self.script
-        self.job = Popen(self.script, stdout=self.out, stderr=self.err)
+
+        for param in os.environ.keys():
+            print "%20s %s" % (param,os.environ[param])
+
+        self.job = Popen(self.script, stdout=self._out, stderr=self._err, cwd=self.homeDir, env=self.env)
         self.pid = self.job.pid
         self.running = True
 
     def hasStarved(self):
-        """An upstream job has failed and
-        this one cannot be reached
+        """Tells this job not to run because an upstream
+        job has failed
         """
 
         self.done=True
         self.running=False
         self.starved=True
+        self.printStats()
 
     def isRunning(self):
         """Is the job running?
         """
         return self.running
 
+    def printStats(self):
+        """Prints the stats of this job to its stats file
+        """
+        pp = pprint.PrettyPrinter(stream=self._stat)
+        statInfo = dict((key, getattr(self, key)) for key in self.statKeys)
+        pp.pprint(statInfo)
+
     def isDone(self):
         """Is the job done?
 
-        Polls the job
-        Grabs return code if its finished
-        Timestamps the endtime
-        Prints the stat file
-        Closes all file handles
+        Steps:
+          - Polls the job
+          - Grabs return code if its finished
+          - Timestamps the endtime
+          - Prints the stat file
+          - Closes all file handles
         """
         #If it's done, it's done
         if self.done is True:
             return self.done
 
-        print '-- Passed the done check'
         #If we're not running and not done,
         #it hasn't even started
         if self.isRunning() is False:
             return False
 
-        print ' -- Passed the isRunning check'
         #Poll the job and grab the ret code
         self.job.poll()
         if self.job.returncode is None:
             return False
 
-        print ' -- Passed the returncode check'
         #We're now done, set the status bits
         self.done = True
         self.running = False
@@ -145,34 +204,6 @@ class Job(object):
         #Print out our stats
         endTS = int(time.time())
         self.endTime = datetime.fromtimestamp(endTS)
-        pp = pprint.PrettyPrinter(stream=self.stat)
-        statInfo = dict((key, getattr(self, key)) for key in self.statKeys)
-        pp.pprint(statInfo)
-
-        #Close the filehandles
-        self.out.close()
-        self.err.close()
-        self.stat.close()
+        self.printStats()
 
         return self.done
-
-    
-
-# j = Job('tst')
-# j1 = Job('tst1')
-# print 'instantiated'
-
-
-# j.run('bob.py')
-# j1.run('test1.py')
-# print 'running'
-
-
-# while not (j.isDone() and j1.isDone()):
-#     if j.isDone():
-#         print 'tst done'
-#     if j1.isDone():
-#         print 'tst1 done'
-#     print 'cycle'
-#     time.sleep(1)
-
---
+++ workflow.py
@@ -0,0 +1,242 @@
+#!/usr/local/bin/python
+
+import networkx
+from distutils.dir_util import mkpath
+from networkx import DiGraph, Graph
+from job import Job
+import time, os, sys
+import yaml
+import pprint
+import commands
+
+class WorkFlow(object):
+    """A collection of directed jobs
+
+    Represents a groups of jobs and their dependencies.  Can run the jobs according
+    to a configuration file and collect their output.  The jobs are stored in a
+    directed acyclical graph (DAG).
+
+    @cvar root: The directory this file is in
+    @type root: C{string}
+    @cvar configRoot: root + 'conf'
+    @type configRoot: C{string}
+    @cvar logRoot: root + 'log'
+    @type logRoot: C{string}
+    @ivar name: Name of this workflow
+    @type name: C{string}
+    @ivar confPath: Path to the configuration file for this flow
+    @type confPath: C{string}
+    @ivar flowLogRoot: Path to the log dir for this flow
+    @type flowLogRoot: C{string}
+    @ivar jobLogRoot: Path to the log dir for all jobs of this flow
+    @type jobLogRoot: C{string}
+    @ivar conf: Complete configuration for this flow
+    @type conf: C{dict}
+    @ivar email: Email for all messages
+    @type email: C{string}
+    @ivar iterPause: Length of pause (in seconds) between walks of the DAG
+    @type iterPause: C{int}    
+    """
+
+    #The current directory of this file
+    root = sys.path[0]
+
+    #The default dir for config files
+    configRoot = os.path.join(root, 'conf')
+
+    #The default dir for log files
+    logRoot = os.path.join(root, 'log')
+
+    def __init__(self, name='default', iterPause=5):
+        """
+        @keyword name: The name of this workflow.  Also determines the
+        name of the conf file to be used and the log directory.  The default
+        value is "default"
+        @type name: C{string}
+        @keyword iterPause: The length of time (in seconds) that the workflow
+        will pause before walking the job graph again, The default is 5 seconds.
+        @type iterPause: C{int}
+
+        """
+        #Conf files, log directories are named after the workflow
+        self.name = name
+        self.confPath = os.path.join(self.configRoot, self.name)
+        self.confPath += '.yml'
+
+        #Start is the starting node of the DAG, it's essentially an
+        #empty node but must be present to walk the graph correctly
+        self.start = None
+
+        #Set up my logging root
+        currTime = int(time.time())
+        self.flowLogRoot = os.path.join(self.logRoot, self.name)
+        self.jobLogRoot = os.path.join(self.flowLogRoot, str(currTime))
+        mkpath(self.jobLogRoot)
+
+        self.pid = os.getpid()
+        self.pidFile = os.path.join(self.flowLogRoot, "%s.pid" % self.name)
+
+        #Some configuration details
+        self.conf = self._loadFlow(self.confPath)
+        self.email = self.conf['email']
+        self.iterPause = iterPause
+
+        #Set up the DAG
+        self.graph = self._buildGraph(self.conf['jobs'])
+        
+    def _loadFlow(self, path):
+        """ Loads the workflow configuration
+        """
+        try:            
+            fYaml = open(path, 'r')
+            conf = yaml.load(fYaml)
+            fYaml.close()
+            return conf
+        except yaml.scanner.ScannerError:
+            print " ** ERROR ** "
+            print "Conf file %s is screwed." % path
+
+    def _buildGraph(self, jobs):
+        """ Builds the workflow graph
+
+        Uses the dependencies specified in the configuration
+        file to build a directed acyclic graph.  This graph
+        is used to direct which process are run and when.
+
+        @param jobs: A list of job attributes and dependencies
+        from the conf file
+        @type jobs: C{list}
+        """
+        h = {}
+
+        #Set up my graph and the dummy start noe
+        g = DiGraph()
+        self.start = Job('start', 'nothin', logDir=self.jobLogRoot)
+        self.start.done = True
+        self.start.succeeded = True
+
+        #Make a dict with the job id as the key
+        #Put the instantiated jobs and dependencies
+        #in as values
+        for node in list(jobs):
+            id = node['id']
+            del node['id']
+            h[id] = {}
+
+            if 'homedir' not in node:
+                node['homedir'] = None
+            env = os.environ
+            if 'env' in node:
+                env.update(node['env'])
+
+            job = Job(id, node['script'], homeDir=node['homedir'], logDir=self.jobLogRoot, env=env)
+            h[id]['job'] = job
+            if 'depends_on' in node:
+
+                #Quack: depends_on must be a list, may be passed
+                #as a string.  This bit of duck typing makes sure
+                #it's a list
+                if not hasattr(node['depends_on'], 'sort'):
+                    node['depends_on'] = [node['depends_on']]
+
+                h[id]['depends_on'] = node['depends_on']
+
+        #Use the dict to build the DAG
+        for jobId, v in h.items():
+            if 'depends_on' in v:
+                for j in v['depends_on']:
+                    g.add_edge(h[j]['job'], v['job'])
+            else:
+                g.add_edge(self.start, v['job'])
+
+        return g
+
+    def run(self):
+        """ Run the workflow
+
+        Will walk the graph every so often until
+        each node has been run or has been declared
+        dead
+
+        A branch is dead when a parent node fatally exits
+        """
+        
+#         if os.path.exists(self.pidFile):
+#             oldPid = file(self.pidFile).read()
+#             sPids = commands.getoutput('ps -eo pid')
+#             pids = sPids.split('\n')
+#             print pids
+#             if oldPid in pids:
+#                 print "Job is currently running"
+#                 exit(0)
+
+#         pidFile = open(self.pidFile, 'w')
+#         pidFile.write(str(self.pid))
+#         pidFile.close()
+
+        done = False
+        while not done:
+            done = self._walk(self.start)
+            time.sleep(self.iterPause)
+
+        latestLink = os.path.join(self.flowLogRoot, 'latest')
+        if os.path.exists(latestLink):
+            os.remove(latestLink)
+        os.symlink(self.jobLogRoot, latestLink)
+        
+    def _walk(self, job):
+        """ Walk the DAG
+
+        Walks the directed graph from root to leaves.  It can
+        only advance to a lower node once the parent has finished.  The child
+        checks if both parents are finished before running itself.  If
+        a node dies fatally, the rest of that branch is declared starved.
+        """
+        # If the current job is done we'll set our local "done" var to true
+        # and then recurse through the child nodes.  Their return values
+        # are anded with this one to see if all nodes have been walked.
+        # If so, we're finished and return True to the run method.
+        if job.isDone():
+            done = True
+            for child in self.graph.successors(job):
+                retVal = self._walk(child)
+                done = done & retVal
+            return done
+
+        #If the job is still running, it's not done
+        if job.isRunning():
+            return False
+    
+        for parent in self.graph.predecessors(job):
+
+            #If the parent isn't done, we can't continue but the
+            #branch is not yet dead
+            if not parent.isDone():
+                return False
+
+            #If this job needs success from its parents and doesn't get
+            #it, the branch is dead, return True (for branch finished)
+            if job.needsSuccess and (not parent.succeeded):
+                self.kill(job) #Kill all children, they've starved
+                return True
+
+        #If it passes the above gauntlet, let's start the process
+        #and pass a False because the branch is not dead
+        job.run()
+        return False
+
+    def kill(self, job):
+        """ Kills the current job as well as all children
+
+        @param job: Job to kill
+        @type job: L{Job}
+        """
+        job.hasStarved()
+        for j in self.graph.successors(job):
+            self.kill(j)
+
+    
+
+#js = WorkFlow('iGoogle')
+#js = WorkFlow()
+#js.run()
